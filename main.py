0292d3c1202fd20349273df6ca22daeb61acb9ec
leetusik <leetusik@gmail.com>
2025-04-17 15:24:06 +0900

    change to uv from pip

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="LangChain Playground",
+    page_icon=":shark:",
+)
+
+st.markdown(
+    """
+# Hello!
+
+Welcome to the LangChain Playground!
+
+Here are the apps I made:
+
+- [] [test](/TestPage)
+
+    """,
+)
