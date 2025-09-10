4dc3273e4604354622ea52f71fbad9c3927b52e0
leetusik <leetusik@gmail.com>
2025-09-10 11:55:59 +0900

    chore: add docker-compose.yml and .gitignore

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_postgres.vectorstores import 
+from langchain_postgres.vectorstores import PGVector
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from langchain_core.documents import Document
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain.storage import InMemoryStore
+
+from pydantic import BaseModel
+import uuid
+import os
+from dotenv import load_dotenv
+
+db_user = os.getenv("POSTGRES_USER")
+db_password = os.getenv("POSTGRES_PASSWORD")
+db_name = os.getenv("POSTGRES_DB")
+db_port = os.getenv("POSTGRES_PORT")
+db_host = os.getenv("POSTGRES_HOST")
+
+connection_string = (
+    f"postgresql+psycopg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+)
+collection_name = "summaries"
+
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
