653bb97f4470d4b177ba1cd9f7d6ea9f09c62b02
leetusik <leetusik@gmail.com>
2025-09-10 11:13:14 +0900

    chore: set up project structure with uv

---
+++ main.py
@@ -0,0 +1,4 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres.vectorstores import 
